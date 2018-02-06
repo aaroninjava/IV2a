b7ead689e0ddf629479b15e8b7aabff52ef730b5
aarontsai <aaroninpcschool241@gmail.com>
2018-02-06 11:44:07 +0800

    新增 OP_value(CP) 方法 與參數

--- src/iv/IV.java
+++ src/iv/IV.java
@@ -10,7 +10,7 @@ public class IV {
             //iv1a();
             //System.out.println("y=x^2, x="+x+",y="+yx(x));
             
-            System.out.println("IV1a"); 
+        System.out.println("IV1a"); 
         String CP="C";        
         //double Pr=1.732;        
         double Pr=0.84;        
@@ -31,7 +31,10 @@ public class IV {
         //System.out.println("call="+b.iv1a());
         //b.iv1a(CP,Pr,S,K,T,r,y);             
         //System.out.println("call="+b.call());                
-        b.iv1a();
+        b.iv1a(CP);
+       
+        //System.out.println("call="+b.call());   
+        //System.out.println("call="+b.OP_value(CP));   
         
     }
     
@@ -75,6 +78,48 @@ public class IV {
           
     }   
         
+    //public  static void iv1(double P) {          
+    public static void iv2() {          
+        
+          //double P=1.414;  
+          double P=1.732;  
+          double value=0;  
+          double err=0;  
+          double tol=0.0001;
+          double sd1=0.01;
+          double sd2=3;  
+          double std=0;   
+          double i=1;   
+          std = (sd1 + sd2) / 2;
+          
+          //value=P*P;
+          value=yx(std);
+          err = Math.abs(value - P);
+          
+         //while (err >= tol)
+          while (err >= tol)//(i<100)
+        {          
+            if (value >= P) {sd2 = std;}
+            if (value < P)  {sd1 = std;}
+            
+             std = (sd1 + sd2) / 2;
+             value=yx(std);          
+             err = Math.abs(value - P);
+             i=i+1;
+             System.out.println("二分逼近計算次數="+i); 
+             System.out.println("二分逼近計算帶入std="+std+",  估計"+value); 
+             
+         }                
+                 
+          
+          System.out.println("y=x*x二分法是"+std);           
+          System.out.println("y=x*x結果是"+value); 
+          System.out.println("結果誤差="+err); 
+          
+          
+    }   
+     
+    
     public  static double yx(double x) {          
           double yx1;
           yx1=x*x;
